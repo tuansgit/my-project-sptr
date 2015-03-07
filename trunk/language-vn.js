551626f7986ba62d38302f748a766ad90aa07dee
tuansgit@gmail.com <tuansgit@gmail.com@ae524fa6-972d-6977-3291-44118dd70221>
2015-03-07 18:08:44 +0000

    nopcart language sptr

---
+++ trunk/language-vn.js
@@ -0,0 +1,31 @@
+﻿//=====================================================================||
+//       NOP Design JavaScript Shopping Cart Language Pack             ||
+//                                                                     ||
+//                      Language Strings                               ||
+//                     ------------------                              ||
+// Strings displayed to end users, in language specific encoding.      ||
+// only modify these strings if you wish to change language specific   ||
+// wording for your site.  If you add a new language, please send it   ||
+// back to NOP Design (http://www.nopdesign.com/forum) so we can add   ||
+// it to the distribution.                                             ||
+//---------------------------------------------------------------------||
+strSorry  = "Xin lỗi, Giỏ hàng Quý khách đã đầy, vui lòng thanh toán.";
+strAdded  = " thêm vào giỏ hàng.";
+strRemove = "Nhấn 'Ok' để bỏ SP này khỏi giỏ.";
+strILabel = "Mã sản phẩm";
+strDLabel = "Tên sản phẩm";
+strQLabel = "Số lượng";
+strPLabel = "Giá";
+strSLabel = "Phí vận chuyển";
+strRLabel = "Bỏ hàng";
+strRButton= "Xóa";
+strSUB    = "Thành tiền";
+strSHIP   = "Vận chuyển";
+strTAX    = "Thuế";
+strTOT    = "Tổng cộng";
+strErrQty = "SL không hợp lệ.";
+strNewQty = 'Nhập SL mới:';
+
+Language = 'vn';
+bLanguageDefined = true;
+
